bea68c106d3ccc85ed872da608552062d45498bc
rptomar <111333993+rptomar@users.noreply.github.com>
2022-12-13 03:41:03 -0800

    this is first cummit

---
+++ src/PostView/PostVIew.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import Card from './Card'
+import Navbar from './Navbar'
+import axios from 'axios'
+
+export default function PostVIew() {
+
+    const [userData, setUserData] = useState('')
+
+    function fetchPosts() {
+        axios
+            .get('https://rohit-instaclone-backend.onrender.com/posts')
+            .then(res => {
+                let dataArr = res.data
+                dataArr.reverse()
+                setUserData(dataArr)
+            })
+    }
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
+
+
+    return (<>
+        <div className='postview-container'>
+            <Navbar />
+            {!userData.length ?
+                <h1 className='while-loading'>Loading Posts...</h1>
+                :
+                <div className='card-caontainer'>
+                    {userData.map((data, idx) => {
+                        return <Card key={idx} data={data} />
+                    })}
+                </div>}
+        </div>
+    </>
+    )
+}
